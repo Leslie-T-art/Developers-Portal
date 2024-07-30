485391fb96a2ff2d7462405cffa04d34b906e18c
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:29:15 +0200

    Create data.ts

---
+++ src/app/shared/heros/data.ts
@@ -0,0 +1,14 @@
+import { Slide } from "./heros.model";
+
+const SLIDES: Slide[] = [
+    {
+        avatar: 'assets/images/avatars/img-6.jpg',
+        description: 'This app is blessing for all professionals!',
+    },
+    {
+        avatar: 'assets/images/avatars/img-8.jpg',
+        description: 'Very convenient to use project manager!',
+    },
+];
+
+export { SLIDES };
