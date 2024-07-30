663e3605abba2faa903c70f2afbc2d028431ff72
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:24:09 +0200

    Create sticky-nav.directive.spec.ts

---
+++ src/app/shared/ui/sticky-nav.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StickyNavDirective } from './sticky-nav.directive';
+
+describe('StickyNavDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StickyNavDirective();
+    expect(directive).toBeTruthy();
+  });
+});
