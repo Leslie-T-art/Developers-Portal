fc0c568ff3047c87f5b06575470058709ea1f003
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:31:18 +0200

    Create data.ts

---
+++ src/app/pages/other/portfolio/shared/data.ts
@@ -0,0 +1,170 @@
+// types
+import { GalleryItem } from "./portfolio.model";
+
+const GRIDGALLERY: GalleryItem[] = [
+    {
+        image: {
+            src: 'assets/images/photos/1.jpg',
+            caption: 'Smart Desk v2.0',
+            thumb: 'assets/images/photos/1.jpg',
+        },
+        title: 'Smart Desk v2.0',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['web'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/7.jpg',
+            caption: 'Task Manager',
+            thumb: 'assets/images/photos/7.jpg',
+        },
+        title: 'Task Manager',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['graphic'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/3.jpg',
+            caption: 'Portfolio Manager',
+            thumb: 'assets/images/photos/3.jpg',
+        },
+        title: 'Portfolio Manager',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['web', 'photography'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/4.jpg',
+            caption: 'Smart Office v2.0',
+            thumb: 'assets/images/photos/4.jpg',
+        },
+        title: 'Smart Office v2.0',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['illustrator'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/5.jpg',
+            caption: 'Online Conference',
+            thumb: 'assets/images/photos/5.jpg',
+        },
+        title: 'Online Conference',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['web', 'photography'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/6.jpg',
+            caption: 'Virtual Receptionist',
+            thumb: 'assets/images/photos/6.jpg',
+        },
+        title: 'Virtual Receptionist',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['graphic'],
+    },
+];
+
+const MASONARYGALLERY: GalleryItem[] = [
+    {
+        image: {
+            src: 'assets/images/photos/9.jpg',
+            caption: 'Smart Desk v2.0',
+            thumb: 'assets/images/photos/9.jpg',
+        },
+        title: 'Smart Desk v2.0',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['graphic'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/14.jpg',
+            caption: 'Iphone App',
+            thumb: 'assets/images/photos/14.jpg',
+        },
+        title: 'Iphone App',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['graphic'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/15.jpg',
+            caption: 'Iphone App2',
+            thumb: 'assets/images/photos/15.jpg',
+        },
+        title: 'Iphone App2',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['photography'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/6.jpg',
+            caption: 'Virtual Receptionist',
+            thumb: 'assets/images/photos/6.jpg',
+        },
+        title: 'Virtual Receptionist',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['graphic'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/5.jpg',
+            caption: 'Task Manager',
+            thumb: 'assets/images/photos/5.jpg',
+        },
+        title: 'Task Manager',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['web'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/12.jpg',
+            caption: 'Task Manager',
+            thumb: 'assets/images/photos/12.jpg',
+        },
+        title: 'Task Manager',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: [''],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/3.jpg',
+            caption: 'Portfolio Manager',
+            thumb: 'assets/images/photos/3.jpg',
+        },
+        title: 'Portfolio Manager',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['photography'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/15.jpg',
+            caption: 'Online Conference',
+            thumb: 'assets/images/photos/15.jpg',
+        },
+        title: 'Online Conference',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['web'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/4.jpg',
+            caption: 'Smart Office v2.0',
+            thumb: 'assets/images/photos/4.jpg',
+        },
+        title: 'Smart Office v2.0',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['illustrator'],
+    },
+    {
+        image: {
+            src: 'assets/images/photos/13.jpg',
+            caption: 'Virtual Receptionist 2',
+            thumb: 'assets/images/photos/13.jpg',
+        },
+        title: 'Virtual Receptionist 2',
+        description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur',
+        category: ['illustrator'],
+    },
+];
+
+export { GRIDGALLERY, MASONARYGALLERY };
