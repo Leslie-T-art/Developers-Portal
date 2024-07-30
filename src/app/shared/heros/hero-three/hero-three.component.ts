9c3adc48104affc3612c51ff8607b8b2ff209749
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:25:44 +0200

    Create hero-three.component.ts

---
+++ src/app/shared/heros/hero-three/hero-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-3',
+  templateUrl: './hero-three.component.html',
+  styleUrls: ['./hero-three.component.scss']
+})
+export class HeroThreeComponent implements OnInit {
+
+  constructor () { }
+
+  ngOnInit(): void {
+  }
+
+}
