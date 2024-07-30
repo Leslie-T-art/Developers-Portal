58a0f7bc3c90f4fe467afef0f10364279782e0ca
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:22:08 +0200

    Create swiper-slider-two.component.ts

---
+++ src/app/shared/widgets/slider/swiper-slider-two/swiper-slider-two.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+// import Swiper core and required modules
+import SwiperCore, { SwiperOptions, Navigation, Autoplay, } from 'swiper';
+
+// types
+import { SliderItem } from '../slider.model';
+
+// install Swiper modules
+SwiperCore.use([Autoplay, Navigation]);
+
+@Component({
+  selector: 'app-widget-swiper-slider-2',
+  templateUrl: './swiper-slider-two.component.html',
+  styleUrls: ['./swiper-slider-two.component.scss']
+})
+export class SwiperSliderTwoComponent implements OnInit {
+
+  @Input() sliderItems: SliderItem[] = [];
+  config: SwiperOptions = {};
+
+  constructor () { }
+
+  ngOnInit(): void {
+    this.config = {
+      loop: true,
+      spaceBetween: 24,
+      autoplay: {
+        delay: 5000
+      },
+      breakpoints: {
+        576: { slidesPerView: 1 },
+        768: { slidesPerView: 2 }
+      },
+      roundLengths: true,
+      navigation: {
+        nextEl: ".swiper-custom-next",
+        prevEl: ".swiper-custom-prev"
+      }
+    }
+  }
+
+}
