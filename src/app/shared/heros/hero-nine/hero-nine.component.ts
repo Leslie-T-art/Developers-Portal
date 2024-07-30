7bb4472f588ae63b9997c26baa203ea45d998f03
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:28:32 +0200

    Create hero-nine.component.ts

---
+++ src/app/shared/heros/hero-nine/hero-nine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-9',
+  templateUrl: './hero-nine.component.html',
+  styleUrls: ['./hero-nine.component.scss']
+})
+export class HeroNineComponent implements OnInit {
+
+  constructor () { }
+
+  ngOnInit(): void {
+  }
+
+}
