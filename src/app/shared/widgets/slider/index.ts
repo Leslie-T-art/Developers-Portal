ee8a6a530e8a79dd2e792266a9cfc890cc90e3ff
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:22:43 +0200

    Create index.ts

---
+++ src/app/shared/widgets/slider/index.ts
@@ -0,0 +1,4 @@
+export { SwiperSliderFourComponent } from './swiper-slider-four/swiper-slider-four.component';
+export { SwiperSliderOneComponent } from './swiper-slider-one/swiper-slider-one.component';
+export { SwiperSliderThreeComponent } from './swiper-slider-three/swiper-slider-three.component';
+export { SwiperSliderTwoComponent } from './swiper-slider-two/swiper-slider-two.component';
