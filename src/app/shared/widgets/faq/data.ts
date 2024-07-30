3806444e6d042db0ad2eb5b9164e73cdfc916c0c
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:23:33 +0200

    Create data.ts

---
+++ src/app/shared/widgets/faq/data.ts
@@ -0,0 +1,23 @@
+// types
+import { FAQItem } from "./faq.model";
+
+const FAQS: FAQItem[] = [
+    {
+        question: 'Can I use this template for my client?',
+        ans: ' Yup, the marketplace license allows you to use this theme in any end products.For more information on licenses, please refere license terms on marketplace.',
+    },
+    {
+        question: 'Can this theme work with WordPress?',
+        ans: "No. This is a HTML template. It won't directly with WordPress, though you can convert this into WordPress compatible theme.",
+    },
+    {
+        question: 'How do I get help with the theme?',
+        ans: 'Use our dedicated support email (support@coderthemes.com) to send your issues or feedback.We are here to help anytime.',
+    },
+    {
+        question: 'Will you regularly give updates of Prompt ?',
+        ans: 'Yes, We will update the Prompt regularly. All the future updates would be available without any cost.',
+    },
+];
+
+export { FAQS }
