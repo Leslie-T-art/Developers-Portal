9bcd3266a0c2bee1098c7ca9e2eb099888a47217
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:31:47 +0200

    Create navigation.component.ts

---
+++ src/app/pages/other/portfolio/portfolio-item/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-item-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class ItemNavigationComponent implements OnInit {
+
+  constructor () { }
+
+  ngOnInit(): void {
+  }
+
+}
