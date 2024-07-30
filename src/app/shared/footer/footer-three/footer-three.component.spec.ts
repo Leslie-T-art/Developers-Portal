5c92b7472608d6aa024609e7cc18de79a60f920e
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:29:29 +0200

    Create footer-three.component.spec.ts

---
+++ src/app/shared/footer/footer-three/footer-three.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterThreeComponent } from './footer-three.component';
+
+describe('FooterThreeComponent', () => {
+  let component: FooterThreeComponent;
+  let fixture: ComponentFixture<FooterThreeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FooterThreeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterThreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
