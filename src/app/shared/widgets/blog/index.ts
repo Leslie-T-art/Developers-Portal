a90a32e08bcc76901afe668071b8bc7fdef2758b
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:23:35 +0200

    Create index.ts

---
+++ src/app/shared/widgets/blog/index.ts
@@ -0,0 +1,3 @@
+export { BlogPostOneComponent } from './blog-post-one/blog-post-one.component';
+export { BlogPostTwoComponent } from './blog-post-two/blog-post-two.component';
+export { BlogPostThreeComponent } from './blog-post-three/blog-post-three.component';
