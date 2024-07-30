dbbd18ea923cf16a2e216bd8e98dda0b47f09493
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:31:24 +0200

    Create portfolio-masonry.component.ts

---
+++ src/app/pages/other/portfolio/portfolio-masonry/portfolio-masonry.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import feather from 'feather-icons';
+import { IAlbum, Lightbox } from 'ngx-lightbox';
+
+// types
+import { GalleryItem } from '../shared/portfolio.model';
+
+// data
+import { MASONARYGALLERY } from '../shared/data';
+
+
+@Component({
+  selector: 'app-portfolio-masonry',
+  templateUrl: './portfolio-masonry.component.html',
+  styleUrls: ['./portfolio-masonry.component.scss']
+})
+export class PortfolioMasonryComponent implements OnInit {
+
+  galleryItems: GalleryItem[] = [];
+  imageCategory: string = 'all';
+  private _albums: IAlbum[] = [];
+
+  constructor (
+    private titleService: Title,
+    private _lightbox: Lightbox
+  ) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle("Prompt - Angular | Portfolio Masonry Example");
+    this._fetchData();
+  }
+
+  ngAfterViewInit(): void {
+    feather.replace()
+  }
+
+  _fetchData(): void {
+    this._albums = [];
+    this.galleryItems = this.imageCategory === 'all' ? MASONARYGALLERY : MASONARYGALLERY.filter((item) => item.category?.includes(this.imageCategory));
+    this.galleryItems.forEach(item => this._albums.push(item.image!));
+  }
+
+  /**
+  * open lightbox
+  * @param index image number
+  */
+  open(index: number): void {
+    this._lightbox.open(this._albums, index, { fadeDuration: 0.1, resizeDuration: 0.1, wrapAround: true, showImageNumberLabel: true, disableScrolling: true, centerVertically: true });
+  }
+
+  /**
+   * close lightbox programmatically
+   */
+  close(): void {
+    this._lightbox.close();
+  }
+
+  /**
+   * filter images by category
+   * @param category category
+   */
+  filterImages(category: string) {
+    this.imageCategory = category;
+    this._fetchData();
+  }
+
+}
