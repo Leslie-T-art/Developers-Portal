8b6aacab98ba143ad8a232f652a073ff8af66f66
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:24:13 +0200

    Create scroll-to-top.directive.spec.ts

---
+++ src/app/shared/ui/scroll-to-top.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScrollToTopDirective } from './scroll-to-top.directive';
+
+describe('ScrollToTopDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ScrollToTopDirective();
+    expect(directive).toBeTruthy();
+  });
+});
