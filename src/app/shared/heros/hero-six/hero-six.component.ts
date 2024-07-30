747b7baac55580113524b27b82ba2f5f2f6eb99f
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:25:58 +0200

    Create hero-six.component.ts

---
+++ src/app/shared/heros/hero-six/hero-six.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-6',
+  templateUrl: './hero-six.component.html',
+  styleUrls: ['./hero-six.component.scss']
+})
+export class HeroSixComponent implements OnInit {
+
+  constructor () { }
+
+  ngOnInit(): void {
+  }
+
+}
