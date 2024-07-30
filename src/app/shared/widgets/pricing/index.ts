4cfcbb2abb94d1cb78e69c839d1ef9261a85e266
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:23:10 +0200

    Create index.ts

---
+++ src/app/shared/widgets/pricing/index.ts
@@ -0,0 +1,2 @@
+export { PricingCardsOneComponent } from "./pricing-cards-one/pricing-cards-one.component";
+export { PricingCardsTwoComponent } from "./pricing-cards-two/pricing-cards-two.component";
