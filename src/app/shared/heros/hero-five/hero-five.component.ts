8c55ea0fa59ab15a955745576df2fedb46ca3aef
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-30 16:28:47 +0200

    Create hero-five.component.ts

---
+++ src/app/shared/heros/hero-five/hero-five.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-5',
+  templateUrl: './hero-five.component.html',
+  styleUrls: ['./hero-five.component.scss']
+})
+export class HeroFiveComponent implements OnInit {
+
+  constructor () { }
+
+  ngOnInit(): void {
+  }
+
+}
