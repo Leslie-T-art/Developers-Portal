2fe7d3b918e3004cc0b2fc691d6268b0ca6674c5
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-08-07 10:58:38 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/prompt-angular'),
+      dir: require('path').join(__dirname, './coverage/quickpi-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
