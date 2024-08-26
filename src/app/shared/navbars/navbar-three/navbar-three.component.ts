59086ff5dc5c1ccf01c6db51307709fef9f05c4b
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-08-26 12:18:26 +0200

    Update navbar-three.component.ts

--- src/app/shared/navbars/navbar-three/navbar-three.component.ts
+++ src/app/shared/navbars/navbar-three/navbar-three.component.ts
@@ -6,6 +6,35 @@ import { AuthenticationService } from 'src/app/core/service/auth.service';
 // types
 import { User } from 'src/app/core/models/auth.models';
 
+interface ProfileOption {
+  icon: string;
+  label: string;
+  redirectTo: string;
+}
+
+const PROFILEOPTIONS: ProfileOption[] = [
+  {
+    icon: 'user',
+    label: 'Profile',
+    redirectTo: '.',
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    redirectTo: '/pages/account/settings',
+  },
+  {
+    icon: 'aperture',
+    label: 'Support',
+    redirectTo: '.',
+  },
+  {
+    icon: 'unlock',
+    label: 'Sign Out',
+    redirectTo: '/auth/logout',
+  },
+];
+
 @Component({
   selector: 'app-navbar-3',
   templateUrl: './navbar-three.component.html',
@@ -21,6 +50,8 @@ export class NavbarThreeComponent implements OnInit {
   showMobileMenu: boolean = false;
 
   loggedInUser: User | null = null;
+ 
+  profileOptions: ProfileOption[] = PROFILEOPTIONS;
 
   constructor (
     private authenticationService: AuthenticationService,
